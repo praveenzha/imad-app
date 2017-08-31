995b44bbf7410e9bd436d007ad4ace8c91afc1dd
praveenzha <31185672+praveenzha@users.noreply.github.com>
2017-08-31 21:11:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,19 @@
 var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function(){
+    var request=new XMLHttepRequest();
+    request.onreadystatechange = function(){
+        if(request.readystate===XML.HttepRequest.Done){
+            if(request.status===200){
+                var counter=request.responsetext;
+                
     counter=counter+1;
     var span=document.getElementById('count');
     span.innerHTML=counter.toString();
-}
+
+            }
+        }
+    }
+    
+};
+    
